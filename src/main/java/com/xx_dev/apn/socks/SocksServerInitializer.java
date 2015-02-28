39edb914ee7f079be53c8799768fe81992c4b0bc
xmxsuperstar <xmxsuperstar@gmail.com>
2015-02-28 15:32:52 +0800

    init

---
+++ src/main/java/com/xx_dev/apn/socks/SocksServerInitializer.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c) 2014 The APN-PROXY Project
+ *
+ * The APN-PROXY Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package com.xx_dev.apn.socks;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.socks.SocksInitRequestDecoder;
+import io.netty.handler.codec.socks.SocksMessageEncoder;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+public final class SocksServerInitializer extends ChannelInitializer<SocketChannel> {
+
+    private final SocksMessageEncoder socksMessageEncoder = new SocksMessageEncoder();
+    private final SocksServerHandler socksServerHandler = new SocksServerHandler();
+
+    @Override
+    public void initChannel(SocketChannel socketChannel) throws Exception {
+        ChannelPipeline p = socketChannel.pipeline();
+        p.addLast("log", new LoggingHandler("BYTE_LOGGER", LogLevel.DEBUG));
+        p.addLast(new SocksInitRequestDecoder());
+        p.addLast(socksMessageEncoder);
+        p.addLast(socksServerHandler);
+    }
+}
